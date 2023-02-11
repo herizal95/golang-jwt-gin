be587bed56a7f334ae7d01705779a03b897ecdf3
herizal95 <alphacsoft.herizal@gmail.com>
2023-02-11 21:00:26 +0800

    setup database

---
+++ config/load_env.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBHost     string `mapstructure:"POSTGRES_HOST"`
+	DBUsername string `mapstructure:"POSTGRES_USERNAME"`
+	DBPassword string `mapstructure:"POSTGRES_PASSWORD"`
+	DBName     string `mapstructure:"POSTGRES_DB"`
+	DBPort     string `mapstructure:"POSTGRES_PORT"`
+
+	ServerPort string `mapstructure:"PORT"`
+
+	TokenSecret    string        `mapstructure:"TOKEN_SECRET"`
+	TokenExpiredIn time.Duration `mapstructure:"TOKEN_EXPIRED_IN"`
+	TokenMaxAge    int           `mapstructure:"TOKEN_MAXAGE"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigFile("env")
+	viper.SetConfigName("app")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
+}
--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/herizal95/golang-jwt-gin/helper"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "welcome golang gin jwt")
+	})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.ErrorPanic(err)
+}
