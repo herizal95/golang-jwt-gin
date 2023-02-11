f289e8cc238153b2f9c79b08551ac4b0cc99b66d
herizal95 <alphacsoft.herizal@gmail.com>
2023-02-11 22:22:54 +0800

    fix jwt

---
+++ utils/jwt.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func GenerateToken(ttl time.Duration, payload interface{}, secretJWTKey string) (string, error) {
+	token := jwt.New(jwt.SigningMethodES256)
+
+	now := time.Now().UTC()
+	claim := token.Claims.(jwt.MapClaims)
+
+	claim["sub"] = payload
+	claim["exp"] = now.Add(ttl).Unix()
+	claim["iat"] = now.Unix()
+	claim["nbf"] = now.Unix()
+
+	tokanstring, err := token.SignedString([]byte(secretJWTKey))
+
+	if err != nil {
+		return "", fmt.Errorf("genereting JWT token failed: %w", err)
+	}
+
+	return tokanstring, err
+}
+
+func ValidateToken(token string, signedJWTKey string) (interface{}, error) {
+	tok, err := jwt.Parse(token, func(jwtToken *jwt.Token) (interface{}, error) {
+		if _, ok := jwtToken.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected method: %s", jwtToken.Header["alg"])
+		}
+
+		return []byte(signedJWTKey), nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("invalid token %w", err)
+	}
+
+	claim, ok := tok.Claims.(jwt.MapClaims)
+	if !ok || !tok.Valid {
+		return nil, fmt.Errorf("invalid token claim")
+	}
+
+	return claim["sub"], nil
+}
